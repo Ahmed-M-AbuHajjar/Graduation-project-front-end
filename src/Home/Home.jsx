cd248b91cfc00ea153e3e1004e52859e42b3df6f
Ahmed Moustafa Abu-Hajjar <74493372+Ahmed-M-AbuHajjar@users.noreply.github.com>
2023-04-13 14:25:29 +0200

    Update Home.jsx

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -21,7 +21,6 @@ export const  Home = () => {
 	);
 	let loggedInUser = localStorage.getItem('user');
 	let userData = JSON.parse(loggedInUser)
-	let role = userData.foundedUser.role;
 
 	
     return(<>
